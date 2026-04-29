44ef7175183c4832581d069c19992203f68790f1
ysktl <yuusky20th@gmail.com>
2026-04-30 02:36:51 +0900

    add trailbase alias

--- .config/zsh/aliases.zsh
+++ .config/zsh/aliases.zsh
@@ -10,4 +10,10 @@ alias sc='scala-cli'
 alias p='podman'
 alias pc-'podman-compose'
 alias oc='opencode'
-
+alias trail='
+  mkdir -p traildepot && \
+  podman run \
+      -p 4000:4000 \
+      -e ADDRESS=0.0.0.0:4000 \
+      --mount type=bind,source="$PWD"/traildepot,target=/app/traildepot \
+      trailbase/trailbase /app/trail'
